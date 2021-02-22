e7e03e79d4ad30bd6148215dd6c7920d6484b046
Madhur Maurya <madhurmaurya365@gmail.com>
2021-02-22 19:24:58 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/infigon_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.infigon_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:infigon_task/presentation/core/app_widget.dart';
+ 
+void main() => runApp(AppWidget());
+ 
